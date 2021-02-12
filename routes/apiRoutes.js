dbbf2a057c3b11b3fe6af5e620929abd5f951023
Harrison Glover <harrison_glover@yahoo.com>
2021-02-12 01:17:44 -0600

    added route for upated stats page and creating new workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,63 @@
 const db = require("../models");
 
-module.exports =(app)=> {
-    app.get("/api/workouts", (req,res) => {
-        db.Workout.find({}).then((dbWorkout) => {
-            
-            db.Workout.aggregate([
-                {
-                    $addFields: {
-                        totalWorkoutDuration:  { $sum:  "$duration"}
-                    }
+module.exports = (app) => {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" }
                 }
-            ]);
+            }
+        ]).then(dbWorkout => {
+            res.json(dbWorkout)
 
-            console.log(dbWorkout);
-            res.json(dbWorkout);
-
-
-            
-        }).catch(err =>{
+        }).catch(err => {
             res.json(err);
         });
 
-        
-        
-            
-        
+
 
     });
 
-     
+    app.put("/api/workouts/:id", ( req , res) => {
+        db.Workout.findByIdAndUpdate(
+            req.params.id,
+            { $push: { exercises: req.body } }
+
+
+        ).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" }
+                }
+
+            }
+        ]).sort({ _id: -1 }).limit(7).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts",(req,res) => {
+
+        db.Workout.create(req.body).then((dbWorkout =>{
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+        });
+
+
+
+
+
 }
